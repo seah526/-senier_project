76b31909bc75aae90467452434581f6451c4eb3c
2je0 <bulljy@naver.com>
2022-11-08 20:23:05 +0900

    🐛 fix(classes): 컴포넌트 key 추가

--- front-end/components/classes/Classes.jsx
+++ front-end/components/classes/Classes.jsx
@@ -7,10 +7,10 @@ const Classes = props => {
       <div className='grid grid-cols-4 gap-4'>
         {props.data?.map(ele => {
           return (
-            <Link href={`/classes/${ele.id || ele.title}`}>
-              <button
-                key={ele.id || ele.title}
-                className='mx-3 bg-slate-500 py-2 px-3 text-lg shadow-inner shadow-gray-700 rounded-md hover:bg-gray-700 hover:text-white'>
+            <Link
+              key={ele.id || ele.title}
+              href={`/classes/${ele.id || ele.title}`}>
+              <button className='mx-3 bg-slate-500 py-2 px-3 text-lg shadow-inner shadow-gray-700 rounded-md hover:bg-gray-700 hover:text-white'>
                 {ele.title}
               </button>
             </Link>
