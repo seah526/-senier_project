07eaa5a5f0294a888899defa1f05de42a75bb4bc
2je0 <bulljy@naver.com>
2022-11-08 20:24:10 +0900

    🧹 chore: 홈화면 필요없는 정보 삭제

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,16 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 export default function Home() {
-  const [data, setData] = useState(null);
-  useEffect(()=>{axios.get('http://localhost:3000/lectures').then(res => {
-    setData(res.data);
-    console.log(res.data);
-  });},[])
+  
   
   return (
     <>
       <h1 className='text-3xl font-bold underline'> </h1>
-      <div>{data?.map(ele => ele.title)}</div>
     </>
   );
 }
