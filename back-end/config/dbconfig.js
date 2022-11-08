96aae23631afc970b1faa54ba8ecf80e0a689aae
seah526 <seah526@naver.com>
2022-11-08 16:32:19 +0900

    feat(backend): backend setting & test api

---
+++ back-end/config/dbconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host     : 'senior-project.cjwwjrzehkte.ap-northeast-2.rds.amazonaws.com',
+    port     : '3306',
+    user     : 'admin',
+    password : 'whfdjqgkffo',
+    database : 'senior_proj'
+  };
---
+++ back-end/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./config/dbconfig');
+
+const connection = mysql.createConnection(dbconfig);
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Root');
+});
+
+app.get('/lectures', (req, res) => {
+  connection.query('SELECT title, type, prof from Lecture', (error, rows) => {
+    if (error) throw error;
+    res.json(rows);
+  });
+});
+
+app.get('/questions/:lecID', (req, res) => {
+    console.log(req.params.lecID);
+    connection.query('SELECT * from Question WHERE ', (err, rows) => {
+        if(err)  throw err;
+        res.json(rows);
+    });
+});
+
+app.get('/answers/:qID', (req, res) => {
+    connection.query('SELECT * from Answer', (err, rows) => {
+        if(err) throw err;
+        res.json(rows);
+    })
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
+});
