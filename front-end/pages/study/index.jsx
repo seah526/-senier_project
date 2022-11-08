0fc6212ba03829a9f4f5d17795499bf0be354bb9
2je0 <bulljy@naver.com>
2022-11-08 23:18:33 +0900

    ✨ feat(study): study 페이지 더미 구현

--- front-end/pages/study/index.jsx
+++ front-end/pages/study/index.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import StudyBox from '../../components/study/StudyBox';
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <div className='text-4xl mt-10 font-bold'>Community</div>
+      <div className='grid grid-cols-4 gap-4 mt-10'>
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+      </div>
+    </>
+  );
 };
 
 export default index;
