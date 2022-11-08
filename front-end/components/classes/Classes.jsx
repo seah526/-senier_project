e6dfc0250e59899fd0c2b2be6c23a9dc1a10f143
2je0 <bulljy@naver.com>
2022-11-08 17:54:19 +0900

    ✨ feat: classes 에 api 연결

--- front-end/components/classes/Classes.jsx
+++ front-end/components/classes/Classes.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import Link from "next/link";
-const Classes = (props) => {
-  const { header, classes } = props.data;
+import React from 'react';
+import Link from 'next/link';
+const Classes = props => {
   return (
     <div className='my-20'>
-      <div className='text-2xl my-5'>{header}</div>
+      <div className='text-2xl my-5'>{props.type}</div>
       <div className='grid grid-cols-4 gap-4'>
-        {classes.map((ele) => {
+        {props.data?.map(ele => {
           return (
-            <Link href={`/classes/${ele.id}`}>
+            <Link href={`/classes/${ele.id || ele.title}`}>
               <button
-                key={ele.id}
+                key={ele.id || ele.title}
                 className='mx-3 bg-slate-500 py-2 px-3 text-lg shadow-inner shadow-gray-700 rounded-md hover:bg-gray-700 hover:text-white'>
-                {ele.subject}
+                {ele.title}
               </button>
             </Link>
           );
