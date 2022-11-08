a6a20cc0ad975e50424e8fdd7a19e19283815d8d
seah526 <seah526@naver.com>
2022-11-08 17:11:36 +0900

    fix(backend): lecture 목록에 id 추가

--- back-end/index.js
+++ back-end/index.js
@@ -12,22 +12,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/lectures', (req, res) => {
-  connection.query('SELECT title, type, prof from Lecture', (error, rows) => {
+  connection.query('SELECT id, title, type, prof from Lecture', (error, rows) => {
     if (error) throw error;
     res.json(rows);
   });
 });
 
 app.get('/questions/:lecID', (req, res) => {
-    console.log(req.params.lecID);
-    connection.query('SELECT * from Question WHERE ', (err, rows) => {
-        if(err)  throw err;
-        res.json(rows);
-    });
+    const lecId  = parseInt(req.params.lecID);
+
+    if (isNaN(lecId)) {
+        console.log("err");
+		// return res.json(stat(400, 'Lecture id must be integer.'));
+	}
+    try{
+        connection.query(`SELECT * from Question WHERE lectureId=${lecId}`, (err, rows) => {
+            if(err) throw err;
+            res.json(rows);
+            console.log(rows);
+        })
+
+    } catch(err){
+		// return res.json(stat(500, err.message));
+    }
+});
+
+app.post('/questions', (req, res) => {
+
 });
 
 app.get('/answers/:qID', (req, res) => {
-    connection.query('SELECT * from Answer', (err, rows) => {
+    const qId = parseInt(req.params.qID);
+
+    connection.query(`SELECT * from Answer WHERE quetionId=${qId}`, (err, rows) => {
         if(err) throw err;
         res.json(rows);
     })
